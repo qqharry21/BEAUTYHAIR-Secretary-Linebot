88b78bd574e2ec189576c68f7d91a3067442d936
HaoChen <qqharry21@gmail.com>
2021-10-22 22:43:55 +0800

    update config

--- src/config/config.js
+++ src/config/config.js
@@ -2,12 +2,33 @@
 
 var mysql2 = require('mysql2');
 
-const db = mysql2.createConnection({
+const pool = mysql2.createPool({
   user: 'b4fc5f71b5dd95',
   host: 'us-cdbr-east-04.cleardb.com',
   password: '7ead9883',
   database: 'heroku_7a65dc3b032abd6',
   multipleStatements: true,
 });
+
+var db = function (sql, options, callback) {
+  console.log(sql, options, callback);
+  if (typeof options === 'function') {
+    callback = options;
+    options = undefined;
+  }
+  pool.getConnection(function (err, conn) {
+    if (err) {
+      callback(err, null, null);
+    } else {
+      conn.query(sql, options, function (err, results, fields) {
+        // callback
+        callback(err, results, fields);
+      });
+      // release connection。
+      // 要注意的是，connection 的釋放需要在此 release，而不能在 callback 中 release
+      conn.release();
+    }
+  });
+};
 
 mysql: module.exports = db;
